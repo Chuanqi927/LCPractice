289dd548d22d53c41c6d8bacb2882a30f602cba4
Chuanqi Xiong <40122680+Edward0o0@users.noreply.github.com>
2020-12-22 19:02:01 -0500

    added treenode class

---
+++ Leetcode/LC1602_FindNearestRightNodeInBinaryTree.java
@@ -0,0 +1,63 @@
+package Leetcode;
+import Leetcode.TreeNode;
+import java.util.*;
+
+public class LC1602_FindNearestRightNodeInBinaryTree {
+    class DFS_Preorder_Solution {
+        int uDepth;
+        TreeNode nextNode, u;
+        public void preorder(TreeNode node, int depth) {
+            // the depth to look for next node is identified
+            if (node == u) {
+                uDepth = depth;
+                // we're on the level to look for the next node
+            } else if (depth == uDepth) {
+                // if this next node is not identified yet
+                if (nextNode == null)
+                    nextNode = node;
+            } else {
+                // continue to traverse the tree
+                if (node.left != null)
+                    preorder(node.left, depth + 1);
+                if (node.right != null)
+                    preorder(node.right, depth + 1);
+            }
+        }
+
+        public TreeNode findNearestRightNode(TreeNode root, TreeNode u) {
+            uDepth = - 1;
+            nextNode = null;
+            this.u = u;
+            preorder(root, 0);
+            return nextNode;
+        }
+    }
+
+    class BFS_Solution {
+        public TreeNode findNearestRightNode(TreeNode root, TreeNode u) {
+            Deque<TreeNode> queue = new ArrayDeque<>();
+            queue.add(root);
+
+            while (!queue.isEmpty()) {
+                for (int i = queue.size(); i > 0; i--) {
+                    TreeNode node = queue.poll();
+
+                    if (node == u) {
+                        if (i == 1) {
+                            return null;
+                        }
+                        return queue.poll();
+                    }
+
+                    if (node.left != null) {
+                        queue.add(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.add(node.right);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
